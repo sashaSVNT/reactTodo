731ec8510fcd4ec5ce8187dab4335e6706639ecd
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2023-10-28 19:07:48 +0300

    feat: Added route todo and methods: GET: return todos from db; POST: create a new todo and save db; PUT: change completed status and update at db; DELETE: remove a todo from db

--- server/index.js
+++ server/index.js
@@ -8,4 +8,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5500;
 
+const todoItemRoute = require('./routes/todoItems');
+
+mongoose.connect(process.env.DB_CONNECT).then(() => console.log("Database connected")).catch(err => console.log(err));
+
 app.listen(PORT, () => console.log("Server connected"));
+
+app.use('/', todoItemRoute);
